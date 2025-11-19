f31e24166589373c06c26011ce405e5492a658a1
amenir88 <ameni.rommene@esprit.tn>
2025-11-19 09:36:46 +0100

    Workshop - Manipulation de composant

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ListSuggestionsComponent } from './core/list-suggestions/list-suggestions.component';
+
+@NgModule({
+  declarations: [ //liste des composants associés à ce module
+    AppComponent,
+    HeaderComponent, FooterComponent, ListSuggestionsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
