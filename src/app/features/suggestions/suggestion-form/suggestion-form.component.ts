83d605e0c8e455ca1b9896d6a36128f6d5271d17
amenir88 <ameni.rommene@esprit.tn>
2025-12-03 12:19:05 +0100

    ReactiveForms

--- src/app/features/suggestions/suggestion-form/suggestion-form.component.ts
+++ src/app/features/suggestions/suggestion-form/suggestion-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-suggestion-form',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class SuggestionFormComponent {
 
+myForm!: FormGroup;
+categories: string[] = [
+'Infrastructure et bâtiments',
+'Technologie et services numériques',
+'Restauration et cafétéria',
+'Hygiène et environnement',
+'Transport et mobilité',
+'Activités et événements',
+'Sécurité',
+'Communication interne',
+'Accessibilité',
+'Autre'
+];
+
+ngOnInit(){
+  this.myForm=new FormGroup({
+    title:new FormControl("", [Validators.required,
+      Validators.minLength(5),Validators.pattern("^[A-Z][a-zA-Z]*$")]),
+    description : new FormControl("",[Validators.required,Validators.minLength(30)]),
+    status:new FormControl("en attente"),
+    category:new FormControl("",Validators.required),
+    date:new FormControl(new Date())
+  })
+}
+add(){
+  console.log(this.myForm.value)
+}
 }
